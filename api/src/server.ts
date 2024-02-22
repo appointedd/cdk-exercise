dfa1da6ae0ec1ff595391904d5e2a8c926ca6d92
Geraint Guan <geraint.guan@icloud.com>
2024-02-22 13:59:27 +0000

    chore: remove TRPC Panel

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,6 @@
 import fastifyCors from "@fastify/cors";
 import { fastifyTRPCPlugin } from "@trpc/server/adapters/fastify";
 import fastify from "fastify";
-import { renderTrpcPanel } from "trpc-panel";
 import { createContext, router } from "./index";
 
 export const server = fastify({
@@ -18,13 +17,4 @@ server.register(fastifyTRPCPlugin, {
     createContext,
     router,
   },
-});
-
-server.get("/trpc-panel", async (req, res) => {
-  res.type("text/html");
-
-  return renderTrpcPanel(router, {
-    url: `${req.headers["x-forwarded-scheme"] ?? "http"}://${req.headers["x-forwarded-host"] ?? req.headers.host ?? "127.0.0.1:3000"}/trpc`,
-    transformer: "superjson",
-  });
 });
